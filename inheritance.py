e643d0df8dababd93f383feae9c3e687aa3d9d35
won jun <nttejun@gmail.com>
2019-01-06 12:42:32 +0900

    Refactoring for remove comments

--- inheritance.py
+++ inheritance.py
@@ -5,6 +5,5 @@ class BoardContentsCls(BoardCls):
     def method1(self): return 'boardContentsClass1'
     def method2(self): return 'boardContentsClass2'
 
-# bicc method1 존재하지 않는다. 부모에서 method1 찾아 리턴
 class BoardInheritanceContentsCls(BoardCls):
     def method2(self): return 'boardInheritanceContentsClass1'
